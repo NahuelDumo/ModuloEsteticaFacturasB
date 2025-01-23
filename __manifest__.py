97138dce01f23c130b61fd81299a746b52f957c7
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2025-01-23 15:31:19 -0300

    Init

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Factura B con IVA discriminado',
+    'version': '16.0.1.0.0',
+    'summary': 'Modifica la apariencia de las facturas tipo B para discriminar el IVA',
+    'category': 'Accounting',
+    'author': 'Dumo Nahuel',
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice_b.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
